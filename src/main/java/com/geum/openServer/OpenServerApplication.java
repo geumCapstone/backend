2a207c31c024a4e6e44994be6f0c0b90822ca66b
pooooobi <cyh4662@icloud.com>
2022-09-16 16:06:25 +0900

    initial setting

---
+++ src/main/java/com/geum/openServer/OpenServerApplication.java
@@ -0,0 +1,13 @@
+package com.geum.openServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geum/openServer/OpenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geum.openServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
